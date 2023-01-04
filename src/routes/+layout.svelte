897f2f547e764b9f82149452f6a749a5cc3cb468
Max <mingxuanlim@gmail.com>
2023-01-04 15:09:01 +0800

    fix: apply to dom element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,12 +27,14 @@
 </script>
 
 {#if $navigating || $manualLoading}
-	<div out:fade="{{ duration: 250 }}" class="fixed top-0 left-0 right-0 bottom-0 z-50 w-full h-screen overflow-hidden flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-900">
-		<Spinner transition:fade="{{ duration: 250 }}" size=12 class="mb-4" />
-		<h5 transition:fade="{{ duration: 250 }}" class="text-2xl font-semibold tracking-tight text-gray-700 dark:text-gray-400 mb-1.5">{loading[Math.floor(Math.random() * loading.length)]}</h5>
-		<p transition:fade="{{ duration: 250 }}" class="w-5/6 font-normal text-gray-700 dark:text-gray-400 leading-tight text-center">
-			<strong>Did you know?</strong> {tips[Math.floor(Math.random() * tips.length)]}
-		</p>
+	<div out:fade={{ duration: 250 }} class="fixed top-0 left-0 right-0 bottom-0 z-50 w-full h-screen overflow-hidden flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-900">
+		<div transition:fade={{ duration: 250 }}>
+			<Spinner size=12 class="mb-4" />
+			<h5 class="text-2xl font-semibold tracking-tight text-gray-700 dark:text-gray-400 mb-1.5">{loading[Math.floor(Math.random() * loading.length)]}</h5>
+			<p class="w-5/6 font-normal text-gray-700 dark:text-gray-400 leading-tight text-center">
+				<strong>Did you know?</strong> {tips[Math.floor(Math.random() * tips.length)]}
+			</p>
+		</div>
 	</div>
 {/if}
 <slot />
