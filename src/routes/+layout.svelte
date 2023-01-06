293c329f491eb5d3ec8e2862b745844fbdb1793b
Max <max@zptx.dev>
2023-01-06 16:07:20 +0800

    refactor: use quotes for size

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,7 @@
 {#if $navigating || $manualLoading}
 	<div out:fade={{ duration: 250 }} class="fixed top-0 left-0 right-0 bottom-0 z-50 w-full h-screen overflow-hidden bg-gray-200 dark:bg-gray-900">
 		<div transition:fade={{ duration: 250 }} class="fixed top-0 left-0 right-0 bottom-0 z-50 w-full h-screen overflow-hidden flex flex-col items-center justify-center">
-			<Spinner size=12 class="mb-4" />
+			<Spinner size="12" class="mb-4" />
 			<h5 class="text-2xl font-semibold tracking-tight text-gray-700 dark:text-gray-400 mb-1.5">{$loadingHeadline[Math.floor(Math.random() * $loadingHeadline.length)]}</h5>
 			<p class="w-5/6 font-normal text-gray-700 dark:text-gray-400 leading-tight text-center">
 				<strong>Did you know?</strong> {$tips[Math.floor(Math.random() * $tips.length)]}
